d0ed88cd433e7350daa14cb27a35bf30980f90bd
Elijah De Guzman <elijahdeguzman@Elijahs-MBP.attlocal.net>
2024-03-07 21:18:39 -0500

    buttons file

--- java.js
+++ java.js
@@ -1,20 +1,23 @@
 
-let main = document.querySelector('main');
+var foo = document.getElementById("foo");
+var bar = document.getElementById("bar");
+var foobar = document.getElementById("foobar");
 
-let joke1 = 'A guy walks into a bar and asks for 1.4 root beers. The bartender says “I’ll have to charge you extra, that’s a root beer float”. The guy says “In that case, better make it a double.';
-let joke2 = "Knock, knock. Who’s There? Very long pause… “Java.”";
-let joke3 = "Programming is 10% writing code and 90% understanding why it’s not working";
+foo.addEventListener('click', function() {
+  var h3 = document.createElement('h3');
+  h3.textContent = 'Foo';
+  document.getElementById('main').appendChild(h3);
+});
 
-let template = `
-  <h1>My Jokes</h1>
-  <ul>
-    <li>${joke1}</li>
-    <li>${joke2}</li>
-    <li>${joke3}</li>
-  </ul>
-`;
 
-main.innerHTML = template;
-let para = document.createElement('p');
-para.textContent = "That's all folks!";
-document.body.appendChild(para);
+bar.addEventListener('click', function() {
+  var h3 = document.createElement('h3');
+  h3.textContent = 'Bar';
+  document.getElementById('main').appendChild(h3);
+});
+
+foobar.addEventListener('click', function() {
+  var h2 = document.createElement('h2');
+  h2.textContent = 'FooBar';
+  document.getElementById('main').appendChild(h2);
+});
